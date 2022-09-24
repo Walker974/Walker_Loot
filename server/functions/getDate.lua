219e1e8b6e888e9a1daac70e109844655ee1ea77
Walker <80550093+Walker974@users.noreply.github.com>
2022-09-24 23:01:51 +0100

    Create get Date function

---
+++ server/functions/getDate.lua
@@ -0,0 +1,19 @@
+--[[
+  This file is part of Walker974 & Tenezia RolePlay.
+  Copyright (c) Walker974 & Tenezia RolePlay - All Rights Reserved
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Walker974
+
+---@param cb function
+_Tenezia_Market_Mission_Server.getDate = function(cb)
+    local date = os.date('*t')
+    local day = os.data('%A')
+    local hour = date.hour + 2
+    local data = {
+        day = day,
+        hour = hour
+    }
+    cb(data)
+end
