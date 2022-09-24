c90cf2641269c55cd7cbbc79df051bd20511864d
Walker <80550093+Walker974@users.noreply.github.com>
2022-09-24 23:04:21 +0100

    Create Caisse Object

---
+++ server/class/_constructor.lua
@@ -0,0 +1,64 @@
+--[[
+  This file is part of Walker974 & Tenezia RolePlay.
+  Copyright (c) Walker974 & Tenezia RolePlay - All Rights Reserved
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Walker974
+
+---@field public id number
+---@field public type string
+---@field public items table
+---@field public allowedJobs table
+---@class _Tenezia_Market_Mission
+_Tenezia_Market_Mission = {}
+_Tenezia_Market_Mission.__index = _Tenezia_Market_Mission
+
+setmetatable(_Tenezia_Market_Mission, {
+    __call = function (_, id, type, items, allowedJobs)
+        local self = setmetatable({}, _Tenezia_Market_Mission)
+        self.id = id
+        self.type = type
+        self.items = items
+        self.allowedJobs = allowedJobs
+        return (self)
+    end
+})
+
+---@type table
+local missions = {}
+
+---@param type string
+---@param items table
+---@param allowedJobs table
+---@return void
+---@public
+function _Tenezia_Market_Mission:createMission(type, items, allowedJobs)
+    if (not (items) or not (allowedJobs)) then
+        return
+    end
+    local id;
+    if (json.encode(missions) == nil) then
+        ---@type number
+         id = 1
+    else
+        ---@type number
+         id = #missions + 1
+    end
+    local mission = _Tenezia_Market_Mission(id, type, items, allowedJobs)
+    table.insert(missions, mission)
+end
+
+---@param id number
+---@return void
+---@public
+function _Tenezia_Market_Mission:DeleteMission(id)
+    if (not (id)) then
+        return
+    end
+    if (not (missions[id])) then
+        print("Mission not found")
+        return
+    end
+    table.remove(missions, id)
+end
