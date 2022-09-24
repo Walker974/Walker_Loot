ffc92a45e90e16b6f0a11d25af8920e92657a727
Walker <80550093+Walker974@users.noreply.github.com>
2022-09-24 23:01:22 +0100

    Init ESX server side

---
+++ server/ESX/ESX_init.lua
@@ -0,0 +1,10 @@
+--[[
+  This file is part of Walker974 & Tenezia RolePlay.
+  Copyright (c) Walker974 & Tenezia RolePlay - All Rights Reserved
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
+--]]
+---@author Walker974
+
+ESX = nil
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
