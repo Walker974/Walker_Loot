29e32ddc21ef0798edc2741600194150e7cfc73d
Walker <80550093+Walker974@users.noreply.github.com>
2022-10-03 14:24:26 +0100

    Create Mission main Server Side

--- server/_main.lua
+++ server/_main.lua
@@ -6,4 +6,41 @@
 --]]
 ---@author Walker974
 
-_Tenezia_Market_Mission_Server = {}
+_Tenezia_Market_Mission_Server = {
+    allowed_players = {
+        Orgas = {},
+        Gangs = {},
+        FO = {}
+    },
+    zones = {
+        Orgas = {
+            {x = 0.0, y = 0.0, z = 0.0, radius = 0.0},
+            {x = 125.0, y = 10.0, z = 12.0, radius = 0.0},
+            {x = 18.0, y = 10.0, z = 12.0, radius = 0.0}
+        },
+        Gangs = {
+            {x = 0.0, y = 0.0, z = 0.0, radius = 0.0},
+            {x = 125.0, y = 10.0, z = 12.0, radius = 0.0},
+            {x = 18.0, y = 10.0, z = 12.0, radius = 0.0}
+        },
+    },
+    Mission = {
+        Orgas = {
+            Allowed = {'mafia', 'cartel'},
+            Rewards = {
+                [1] = {'bread', 'water'},
+                [2] = {'burger', 'bandage'},
+                [3] = {'coke', 'weed'}
+            }
+        },
+        Gangs = {
+            Allowed = {'ballas', 'vagos'},
+            Rewards = {
+                [1] = {'bread', 'water'},
+                [2] = {'burger', 'bandage'},
+                [3] = {'coke', 'weed'}
+            }
+        },
+        FO = {'police', 'fbi'}
+    }
+}
