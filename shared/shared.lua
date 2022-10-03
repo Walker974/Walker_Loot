0cbf298acf9716de52f8aa1d4a37e0263e7bf184
Walker <80550093+Walker974@users.noreply.github.com>
2022-10-03 14:27:44 +0100

    Update Shared

--- shared/shared.lua
+++ shared/shared.lua
@@ -1,7 +1,7 @@
 --[[
-  This file is part of Walker Framework.
-  Created at 24/09/2022 18:34
-  
-  Copyright (c) Walker Framework - All Rights Reserved
+  This file is part of Walker974 & Tenezia RolePlay.
+  Copyright (c) Walker974 & Tenezia RolePlay - All Rights Reserved
+  Unauthorized using, copying, modifying and/or distributing of this file,
+  via any medium is strictly prohibited. This code is confidential.
 --]]
 ---@author Walker974
