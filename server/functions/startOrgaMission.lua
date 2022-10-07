1a97f427624fb62f89f382ff94e9a46b8dd13dac
Walker <80550093+Walker974@users.noreply.github.com>
2022-10-07 17:15:05 +0100

    Create Start Orga Mission Function

--- server/functions/startOrgaMission.lua
+++ server/functions/startOrgaMission.lua
@@ -5,3 +5,12 @@
   via any medium is strictly prohibited. This code is confidential.
 --]]
 ---@author Walker974
+
+_Tenezia_Market_Mission_Server.StartOrgaMission = function()
+    local mission = _Tenezia_Market_Mission:createMission('Orgas', _Tenezia_Market_Mission_Server.getRandomOrgaReward(), _Tenezia_Market_Mission_Server.getRandomZoneOrga(), _Tenezia_Market_Mission_Server.Mission.Orgas.Allowed);
+    local get_mission = _Tenezia_Market_Mission:getMission(mission)
+    _Tenezia_Market_Mission_Server.CreateObject_orgas("prop_boxpile_07d", get_mission.position, get_mission.id)
+    _Tenezia_Market_Mission_Server.Send_Orga_Notif_Begin(get_mission.id)
+    Wait(5000)
+    _Tenezia_Market_Mission_Server.Send_FO_Notif_Begin(get_mission.id)
+end
