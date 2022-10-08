bccebab8dd2865b95ddddebf0394af9035f7b36a
Walker <80550093+Walker974@users.noreply.github.com>
2022-10-08 22:57:50 +0100

    move & Add Blips Config

--- client/_main.lua
+++ client/_main.lua
@@ -7,4 +7,18 @@
 ---@author Walker974
 
 ---@class _Tenezia_Market_Mission_Client
-_Tenezia_Market_Mission_Client = {}
+_Tenezia_Market_Mission_Client = {
+    Blips = {
+        sprite = 303,
+        color = 1,
+        scale = 0.8,
+        display = 4,
+        name = 'Largage',
+        Stock = {}
+    },
+    Blips_For_Radius = {
+        color = 1,
+        alpha = 75,
+        Stock = {}
+    }
+}
